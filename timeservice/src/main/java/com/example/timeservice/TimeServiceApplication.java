16799a027247ad956065ee80c8fc935bae459f6e
hemraj <hemraj@oliveglobal.com>
2020-04-02 09:25:24 +0530

    29-12-2019

--- timeservice/src/main/java/com/example/timeservice/TimeServiceApplication.java
+++ timeservice/src/main/java/com/example/timeservice/TimeServiceApplication.java
@@ -13,12 +13,25 @@ import java.util.Map;
 @RestController
 public class TimeServiceApplication {
 
+    @RequestMapping("/")
+    public Map<String, String> Ok(){
+        Map<String,String> response=new HashMap<String, String>(){{
+            this.put("status","OK");
+        }};
+        return response;
+    }
+
     @RequestMapping("/time-service")
     public Map<String, String> currentTime(){
         Map<String,String> response=new HashMap<String, String>(){{
             this.put("status","OK");
             this.put("time","Today is "+new Date());
         }};
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return response;
     }
 
